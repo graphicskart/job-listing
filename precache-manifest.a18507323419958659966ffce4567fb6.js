488552a1e9cde9f5043d69d8bfa3f97f5450dcf4
graphicskart <graphicskart2018@gmail.com>
2019-03-04 12:32:48 +0530

    Updates

---
+++ precache-manifest.a18507323419958659966ffce4567fb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f580e8429a821a3434cd",
+    "url": "/job-listing/static/css/main.4fa25a0a.chunk.css"
+  },
+  {
+    "revision": "f580e8429a821a3434cd",
+    "url": "/job-listing/static/js/main.f580e842.chunk.js"
+  },
+  {
+    "revision": "071f5dfbb9125102b0ea",
+    "url": "/job-listing/static/js/runtime~main.071f5dfb.js"
+  },
+  {
+    "revision": "5679437ad29695d93021",
+    "url": "/job-listing/static/js/2.5679437a.chunk.js"
+  },
+  {
+    "revision": "15b3c34217117154fb1e6f331fddf13a",
+    "url": "/job-listing/index.html"
+  }
+];
